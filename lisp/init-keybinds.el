4a0b6108d145fd7f4a4fa2cd743d30b6bf5db25d
Chaochao Cui <cuichaochao916@gmail.com>
2020-11-13 19:38:05 +0800

    Update init-keybinds.el

--- lisp/init-keybinds.el
+++ lisp/init-keybinds.el
@@ -14,14 +14,15 @@
 
 
 ;;; ivy
+(global-set-key (kbd "C-s") 'swiper)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "C-x b") 'ivy-switch-buffer)
 
 ;;; projectile keybinds
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-(define-key projectile-command-map (kbd "a") #'projectile-add-known-project)
-(define-key projectile-command-map (kbd "A") #'counsel-projectile-find-other-file)
+(define-key projectile-command-map (kbd "A") #'projectile-add-known-project)
+(define-key projectile-command-map (kbd "a") #'projectile-find-other-file)
 (define-key projectile-command-map (kbd "f") #'counsel-projectile-find-file)
 (define-key projectile-command-map (kbd "p") #'counsel-projectile-switch-project)
 
