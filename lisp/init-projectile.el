cb5b60b5579e81552710410a2fc085f61945770a
Chaochao Cui <cuichaochao916@gmail.com>
2020-11-16 20:49:20 +0800

    Update init-projectile.el

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -28,12 +28,19 @@
 ;;(setq projectile-tags-command "etag")
 (setq projectile-tags-backend 'auto)
 
-(setq projectile-globally-ignored-files '("TAGS"))
+(add-to-list 'projectile-globally-ignored-files "TAGS")
 (setq projectile-globally-ignored-file-suffixes '("patch" "*." ".elcz"))
 (add-to-list 'projectile-globally-ignored-modes "dashboard-mode")
-
+(setq projectile-generic-command "fdfind . -0 --type f --color=never")
 (setq projectile-cache-file (expand-file-name "~/.cache/projectile/projectile.cache"))
 (setq projectile-known-projects-file (expand-file-name "~/.cache/projectile/projectile.projects"))
+
+(setq counsel-projectile-grep-initial-input  '(ivy-thing-at-point))
+(setq counsel-projectile-ag-initial-input  '(ivy-thing-at-point))
+(setq counsel-projectile-rg-initial-input  '(ivy-thing-at-point))
+
+(setq counsel-projectile-sort-files t)
+(setq counsel-projectile-sort-buffers t)
 
 (projectile-mode +1)
 (counsel-projectile-mode +1)
